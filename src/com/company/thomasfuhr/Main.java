7e75af2b972f4d689094eefe5a0bc8446d7f4254
Thomas Fuhr <thomas@fuhr.dev>
2021-01-27 21:46:00 +0100

    - added initial adding german as language

--- src/com/company/thomasfuhr/Main.java
+++ src/com/company/thomasfuhr/Main.java
@@ -1,5 +1,7 @@
 package com.company.thomasfuhr;
 
+import com.company.thomasfuhr.database.Database;
+import com.company.thomasfuhr.datatypes.Language;
 import com.company.thomasfuhr.translationMemory.TranslationMemory;
 
 import java.io.IOException;
@@ -7,6 +9,9 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
+
+        // create german because its based on german search and creating, so german has to exist
+        Database.getInstance().addLanguage(new Language("Deutsch"));
 
         // create new instance of translationMemory and start the application
         TranslationMemory translationMemory = new TranslationMemory();
